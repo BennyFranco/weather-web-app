b540bf712706a4f557475a3ab19e32765d3e84b6
Benny Franco Dennis <bennyfranco@sandcode.com>
2016-03-27 22:54:55 -0700

    Some style  corrections

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var db;
+var MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb://localhost:27017/ClimaPIEAES', function(err, dbc) {
+  if (err) {
+    throw err;
+  }
+  db = dbc;
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 router.get('/station/:name', function(req, res) {
-  var MongoClient = require('mongodb').MongoClient;
-
-  MongoClient.connect('mongodb://localhost:27017/ClimaPIEAES', function(err, db) {
-    if (err) {
-      throw err;
-    }
 
     db.collection(req.params.name).find({}).toArray(function(err, result) {
       if (err) {
@@ -27,41 +30,30 @@ router.get('/station/:name', function(req, res) {
         dataDateTime.push(result[i].DateTime);
         stationSensors.push(result[i].sensores);
       }
-      res.render('jazmin', {
+      res.render('station', {
         "stationName" : stationName, "dataDateTime":dataDateTime, "stationSensors":stationSensors
       });
-      //console.log(result);
     });
-  });
 });
 
 router.get('/station/:name/:date', function(req, res) {
-  var MongoClient = require('mongodb').MongoClient;
-
-  MongoClient.connect('mongodb://localhost:27017/ClimaPIEAES', function(err, db) {
-    if (err) {
-      throw err;
-    }
-
     db.collection(req.params.name).find({"DateTime":req.params.date}).toArray(function(err, result) {
       if (err) {
         throw err;
       }
 
-      var stationName = result[0].name
-      var dataDateTime = []
-      var stationSensors = []
+      var stationName = req.params.name;
+      var dataDateTime = [];
+      var stationSensors = [];
 
       for (i = 0; i < result.length; i++) {
         dataDateTime.push(result[i].DateTime);
         stationSensors.push(result[i].sensores);
       }
-      res.render('jazmin', {
+      res.render('station', {
         "stationName" : stationName, "dataDateTime":dataDateTime, "stationSensors":stationSensors
       });
-      //console.log(result);
     });
-  });
 });
 
 module.exports = router;
